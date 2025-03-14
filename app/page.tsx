356ace157a97cfd6c8a885c861a8f9b4fdf956a5
Vlad <vblanton@gmail.com>
2025-03-14 18:09:37 +0100

    background colors lightened

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-[radial-gradient(circle_at_15%_0%,#412c63_-10%,#9b4ed9_75%)] dark:bg-[radial-gradient(circle_at_15%_0%,#2d1c47_-10%,#412c63_75%)] min-h-screen transition-colors duration-200 flex flex-col">
+    <div className="bg-[radial-gradient(circle_at_15%_0%,#412c63_-10%,#9b4ed9_75%)] dark:bg-[radial-gradient(circle_at_15%_0%,#331f4d_-10%,#7a3499_75%)] min-h-screen transition-colors duration-200 flex flex-col">
       <Header />
       <main className="w-full max-w-screen-xl mx-auto py-8 flex-grow">
         <AuthOrSearchSection />
